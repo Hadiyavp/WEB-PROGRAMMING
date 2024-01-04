470c755679f4349baf7d2f0fab251aca09b45c0c
Hadiyavp <147604438+Hadiyavp@users.noreply.github.com>
2024-01-04 12:15:01 +0500

    Completed

---
+++ php/sample.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php
+echo "My First PHP
+script!";
+?>
+</body>
+</html>
+
---
+++ php/summ.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php
+$arr=array(1,8,10,23,5,28,67,9,2,6);
+$total=array_sum($arr);
+echo $total;
+?>
+</body>
+</html>
