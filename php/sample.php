867abaeaa7abfce08450e7d92cfebdc974cfbc10
Hadiyavp <147604438+Hadiyavp@users.noreply.github.com>
2024-01-04 12:45:22 +0530

    Delete php directory

--- php/sample.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<?php
-echo "My First PHP
-script!";
-?>
-</body>
-</html>
-
--- php/summ.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<?php
-$arr=array(1,8,10,23,5,28,67,9,2,6);
-$total=array_sum($arr);
-echo $total;
-?>
-</body>
-</html>
